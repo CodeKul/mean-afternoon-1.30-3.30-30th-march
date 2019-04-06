22131a14ae085f6b4ead39043e712ca76a5b6596
aniruddha <aniruddha.kudalkar@gmail.com>
2019-04-06 12:51:27 +0530

    express : setup, request params and pug templating

---
+++ project-str/http/simple.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const app = express()
+app.set('views', './views')
+app.set('view engine', 'pug')
+
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.send(`
+        <html>
+            <head>
+            <title> My Name </title>
+            </head>
+            <body>
+                <h1> Your Name is ${req['query']['myNm']}</h1>
+            </body>
+        </html>
+
+    `)
+})
+app.get('/my', (req, res) => {
+    res.render('my', { title: 'Android', myNm: req['query']['myNm'] })
+})
+
+app.listen(port, () => console.log(`Express Server Running on port ${port}`))
