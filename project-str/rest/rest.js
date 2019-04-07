080ab960fdb71ec4b5a3376bcdb0950c0aa724de
aniruddha <aniruddha.kudalkar@gmail.com>
2019-04-07 13:19:35 +0530

    rest : invoice pdf creation

--- project-str/rest/rest.js
+++ project-str/rest/rest.js
@@ -16,12 +16,32 @@ app.get(`/account`, (req, res) => {
     res.json(ac)
 })
 
+app.post('/invoice', (req, res) => {
+    let invDt = req.body
+    let htmlInvoice = `
+    <html>
+        <head>
+            <title> My Name </title>
+        </head>
+        <body>
+             <h1> Customer Name Name is ${invDt['custNm']}</h1>
+        </body>
+    </html>
+    `
+    fs.writeFile('./inv.html', htmlInvoice, err =>{
+        if(err) {
+               //send res 
+        }
+        //covert html to pdf and send json response
+    })
+})
+
 function getAccount() {
     return {
-        no : '989856',
-        bal : 8989,
-        lastupdate : new Date(),
-        nm : 'android'
+        no: '989856',
+        bal: 8989,
+        lastupdate: new Date(),
+        nm: 'android'
     }
 }
 app.listen(port, () => console.log(`Express Server Running on port ${port}`))
